70d36a2cf6240bf31e6dff93edd84d80e78bfda3
Anton Timmermans <email@atimmer.com>
2018-08-30 16:24:19 +0200

    Release 8.2-rc2

--- js/src/wp-seo-analysis-worker.js
+++
@@ -1,5 +0,0 @@
-import "babel-polyfill";
-import AnalysisWebWorker from "yoastseo/js/worker/AnalysisWebWorker";
-
-const worker = new AnalysisWebWorker( self );
-worker.register();
--- js/tests/setupTests.js
+++
@@ -1,5 +0,0 @@
-import "raf/polyfill";
-import Enzyme from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
-
-Enzyme.configure( { adapter: new EnzymeAdapter() } );
