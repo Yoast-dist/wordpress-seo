4bcc1b18dac9d0e596d05ba2eef1c429a1965648
Travis CI User <travis@example.org>
2020-05-29 14:46:22 +0000

    Merge pull request #15316 from Yoast/js-file-structure-1

--- js/dist/wp-seo-filter-explanation-142-RC3.js
+++
@@ -1,1 +0,0 @@
-(window.yoastWebpackJsonp=window.yoastWebpackJsonp||[]).push([[16],{807:function(t,i,o){"use strict";jQuery("#posts-filter .tablenav.top").after('<div class="notice notice-info inline wpseo-filter-explanation"><p class="dashicons-before dashicons-lightbulb">'+yoastFilterExplanation.text+"</p></div>")}},[[807,0]]]);
