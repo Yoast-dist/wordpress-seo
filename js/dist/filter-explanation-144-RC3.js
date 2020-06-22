a9bcccb66ccfd837b8a70ee16cebfa38910cbe18
Travis CI User <travis@example.org>
2020-06-22 15:25:35 +0000

    Merge remote-tracking branch 'origin/trunk' into feature/internal-linking-phase-2

--- js/dist/filter-explanation-144-RC3.js
+++
@@ -1,1 +0,0 @@
-(window.yoastWebpackJsonp=window.yoastWebpackJsonp||[]).push([[7],{756:function(t,i,o){"use strict";jQuery("#posts-filter .tablenav.top").after('<div class="notice notice-info inline wpseo-filter-explanation"><p class="dashicons-before dashicons-lightbulb">'+yoastFilterExplanation.text+"</p></div>")}},[[756,0]]]);
