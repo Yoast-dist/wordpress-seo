f8227b9337787dc119ab998e6377ea962c2c5ff2
Travis CI User <travis@example.org>
2020-07-08 12:52:45 +0000

    Merge pull request #15630 from Yoast/P3-76-implement-premium-check
    Added proper check for whether Premium is active or not.

--- languages/wordpress-seojs.php
+++ languages/wordpress-seojs.php
@@ -277,7 +277,7 @@ $generated_i18n_strings = array(
 	// Reference: js/src/help-scout-beacon.js:208
 	__( 'When you click OK we will open our HelpScout beacon where you can find answers to your questions. This beacon will load our support data and also potentially set cookies.', 'wordpress-seo' ),
 
-	// Reference: js/src/initializers/edit.js:218
+	// Reference: js/src/initializers/edit.js:222
 	__( 'Yoast SEO', 'wordpress-seo' ),
 
 	// Reference: js/src/initializers/edit.js:87
