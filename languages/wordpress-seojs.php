44e8032cf1a89651b448c1f7d28f71c314521fbf
Travis CI User <travis@example.org>
2020-10-07 10:33:11 +0000

    Merge branch 'release/15.1' into trunk

--- languages/wordpress-seojs.php
+++ languages/wordpress-seojs.php
@@ -90,22 +90,25 @@ $generated_i18n_strings = array(
 	// Reference: js/src/components/PrePublish.js:48
 	__( 'Improve your post with Yoast SEO', 'wordpress-seo' ),
 
-	// Reference: js/src/components/Indexation.js:182
-	__( 'This button to optimize the SEO data for your website is disabled for non-production environments.', 'wordpress-seo' ),
+	// Reference: js/src/components/Indexation.js:211
+	__( 'This feature includes and replaces the Text Link Counter and Internal Linking Analysis', 'wordpress-seo' ),
 
-	// Reference: js/src/components/Indexation.js:188
+	// Reference: js/src/components/Indexation.js:233
+	__( 'SEO data optimization is disabled for non-production environments.', 'wordpress-seo' ),
+
+	// Reference: js/src/components/Indexation.js:239
 	__( 'SEO data optimization complete', 'wordpress-seo' ),
 
-	// Reference: js/src/components/Indexation.js:202
+	// Reference: js/src/components/Indexation.js:253
 	__( 'Optimizing SEO data... This may take a while.', 'wordpress-seo' ),
 
-	// Reference: js/src/components/Indexation.js:208
+	// Reference: js/src/components/Indexation.js:259
 	__( 'Oops, something has gone wrong and we couldn\'t complete the optimization of your SEO data. Please click the button again to re-start the process.', 'wordpress-seo' ),
 
-	// Reference: js/src/components/Indexation.js:218
+	// Reference: js/src/components/Indexation.js:270
 	__( 'Stop SEO data optimization', 'wordpress-seo' ),
 
-	// Reference: js/src/components/Indexation.js:224
+	// Reference: js/src/components/Indexation.js:276
 	__( 'Start SEO data optimization', 'wordpress-seo' ),
 
 	// Reference: js/src/components/LocalSEOUpsell.js:76
