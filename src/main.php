2dc747283430e78c30076b12eb646a025392490a
Travis CI User <travis@example.org>
2021-01-07 14:12:43 +0000

    Release 15.6-RC5

--- src/main.php
+++ src/main.php
@@ -2,7 +2,7 @@
 
 namespace Yoast\WP\SEO;
 
-use Yoast\WP\Lib\Abstract_Main;
+use Exception;
 use Yoast\WP\SEO\Dependency_Injection\Container_Compiler;
 use Yoast\WP\SEO\Generated\Cached_Container;
 use Yoast\WP\SEO\Surfaces\Classes_Surface;
@@ -22,7 +22,7 @@ if ( ! \defined( 'WPSEO_VERSION' ) ) {
  * @property Meta_Surface    $meta         The meta surface.
  * @property Helpers_Surface $helpers      The helpers surface.
  */
-class Main extends Abstract_Main {
+class Main {
 
 	/**
 	 * The API namespace constant.
@@ -38,20 +38,91 @@ class Main extends Abstract_Main {
 	 */
 	const WP_CLI_NAMESPACE = 'yoast';
 
-	// @phpcs:disable Generic.Commenting.DocComment.MissingShort -- Description is included in the inherited comment.
+	/**
+	 * The DI container.
+	 *
+	 * @var Cached_Container|null
+	 */
+	private $container;
 
 	/**
-	 * @inheritDoc
+	 * Surface classes that provide our external interface.
+	 *
+	 * @var string[]
 	 */
-	protected function get_container() {
+	private $surfaces = [
+		'classes' => Classes_Surface::class,
+		'meta'    => Meta_Surface::class,
+		'helpers' => Helpers_Surface::class,
+	];
+
+	/**
+	 * Loads the plugin.
+	 *
+	 * @throws Exception If loading fails and YOAST_ENVIRONMENT is development.
+	 */
+	public function load() {
+		if ( $this->container ) {
+			return;
+		}
+
+		try {
+			$this->container = $this->get_container();
+
+			if ( ! $this->container ) {
+				return;
+			}
+
+			$this->container->get( Loader::class )->load();
+		} catch ( Exception $e ) {
+			if ( $this->is_development() ) {
+				throw $e;
+			}
+			// Don't crash the entire site, simply don't load.
+			// TODO: Add error notifications here.
+		}
+	}
+
+	/**
+	 * Magic getter for retrieving a property.
+	 *
+	 * @param string $property The property to retrieve.
+	 *
+	 * @throws Exception When the property doesn't exist.
+	 *
+	 * @return string The value of the property.
+	 */
+	public function __get( $property ) {
+		if ( isset( $this->{$property} ) ) {
+			$this->{$property} = $this->container->get( $this->surfaces[ $property ] );
+
+			return $this->{$property};
+		}
+		throw new Exception( "Property $property does not exist." );
+	}
+
+	/**
+	 * Checks if the given property exists as a surface.
+	 *
+	 * @param string $property The property to retrieve.
+	 *
+	 * @return bool True when property is set.
+	 */
+	public function __isset( $property ) {
+		return isset( $this->surfaces[ $property ] );
+	}
+
+	/**
+	 * Loads the DI container.
+	 *
+	 * @throws Exception If something goes wrong generating the DI container.
+	 *
+	 * @return null|Cached_Container The DI container.
+	 */
+	private function get_container() {
 		if ( $this->is_development() && \class_exists( '\Yoast\WP\SEO\Dependency_Injection\Container_Compiler' ) ) {
 			// Exception here is unhandled as it will only occur in development.
-			Container_Compiler::compile(
-				$this->is_development(),
-				__DIR__ . '/generated/container.php',
-				__DIR__ . '/../config/dependency-injection/services.php',
-				'Yoast\WP\SEO\Generated'
-			);
+			Container_Compiler::compile( $this->is_development() );
 		}
 
 		if ( \file_exists( __DIR__ . '/generated/container.php' ) ) {
@@ -64,20 +135,11 @@ class Main extends Abstract_Main {
 	}
 
 	/**
-	 * @inheritDoc
+	 * Returns whether or not we're in an environment for Yoast development.
+	 *
+	 * @return bool Whether or not to load in development mode.
 	 */
-	protected function get_name() {
-		return 'yoast-seo';
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	protected function get_surfaces() {
-		return [
-			'classes' => Classes_Surface::class,
-			'meta'    => Meta_Surface::class,
-			'helpers' => Helpers_Surface::class,
-		];
+	private function is_development() {
+		return \defined( 'YOAST_ENVIRONMENT' ) && \YOAST_ENVIRONMENT === 'development';
 	}
 }
