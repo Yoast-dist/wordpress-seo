51c202ff0cea1bc7fdd4c88cc37e72436bd2f605
Travis CI User <travis@example.org>
2021-07-19 17:56:28 +0000

    Release 16.7.1-RC1

--- src/schema-templates/cooking-time.schema.php
+++ src/schema-templates/cooking-time.schema.php
@@ -1,3 +1,3 @@
 <?php // phpcs:ignore Internal.NoCodeFound ?>
 {{schema name="yoast/cooking-time" only-nested=true }}
-{{attribute name="cooking-time-iso8601-duration" }}
+{{attribute name="iso8601Duration" }}
