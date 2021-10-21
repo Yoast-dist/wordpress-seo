90fb635d6398ad7e382f325a8501481b77f17a15
Travis CI User <travis@example.org>
2021-10-21 11:34:28 +0000

    Merge pull request #17529 from Yoast/LINGO-1075-short-links-in-prominent-words-and-prominent-words-do-not-use-tracking-params
    Added link to research article to word occurrence insights component …

--- languages/yoast-components.php
+++ languages/yoast-components.php
@@ -51,16 +51,16 @@ $generated_i18n_strings = array(
 	// Reference: packages/components/src/Toggle.js:146
 	__( 'Off', 'wordpress-seo' ),
 
-	// Reference: packages/components/src/WordOccurrenceInsights.js:21
+	// Reference: packages/components/src/WordOccurrenceInsights.js:23
 	__( 'Read our %1$sultimate guide to keyword research%2$s to learn more about keyword research and keyword strategy.', 'wordpress-seo' ),
 
-	// Reference: packages/components/src/WordOccurrenceInsights.js:47
+	// Reference: packages/components/src/WordOccurrenceInsights.js:49
 	__( 'Once you add a bit more copy, we\'ll give you a list of words that occur the most in the content. These give an indication of what your content focuses on.', 'wordpress-seo' ),
 
-	// Reference: packages/components/src/WordOccurrenceInsights.js:54
+	// Reference: packages/components/src/WordOccurrenceInsights.js:56
 	__( 'The following words occur the most in the content. These give an indication of what your content focuses on. If the words differ a lot from your topic, you might want to rewrite your content accordingly. ', 'wordpress-seo' ),
 
-	// Reference: packages/components/src/WordOccurrenceInsights.js:72
+	// Reference: packages/components/src/WordOccurrenceInsights.js:74
 	// Reference: packages/components/src/data-model/DataModel.js:50
 	__( 'Prominent words', 'wordpress-seo' ),
 
