6f0fb4acc9f2008158bc46a9c039d8f68b1f85ff
Travis CI User <travis@example.org>
2021-12-06 10:56:29 +0000

    Merge pull request #17776 from Yoast/DUPP-138-add-cards
    DUPP-138 add cards

--- languages/wordpress-seojs.php
+++ languages/wordpress-seojs.php
@@ -750,6 +750,27 @@ $generated_i18n_strings = array(
 	// Reference: packages/js/src/inline-links/inline.js:229
 	__( 'Open in new tab', 'wordpress-seo' ),
 
+	// Reference: packages/js/src/installation-success.js:22
+	__( 'Your site is now easy to find for search engines!', 'wordpress-seo' ),
+
+	// Reference: packages/js/src/installation-success.js:26
+	/* %s expands to Yoast SEO. */
+	__( '%s rolls out the red carpet for the search bots, which helps your site perform better in search engines.', 'wordpress-seo' ),
+
+	// Reference: packages/js/src/installation-success.js:35
+	__( 'Man holding a tablet.', 'wordpress-seo' ),
+
+	// Reference: packages/js/src/installation-success.js:43
+	/* %s expands to Yoast SEO. */
+	__( 'Configure %s!', 'wordpress-seo' ),
+
+	// Reference: packages/js/src/installation-success.js:50
+	/* %s expands to Yoast SEO. */
+	__( 'Set the essential %s settings in a few steps.', 'wordpress-seo' ),
+
+	// Reference: packages/js/src/installation-success.js:64
+	__( 'Start configuration workout!', 'wordpress-seo' ),
+
 	// Reference: packages/js/src/structured-data-blocks/faq/block.js:21
 	__( 'Yoast FAQ', 'wordpress-seo' ),
 
