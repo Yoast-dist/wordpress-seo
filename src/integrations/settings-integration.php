17257fc00fc89727ea0994a0c92d9d558544924d
GitHub Action <igorschoester@users.noreply.github.com>
2022-07-13 17:37:54 +0000

    Syncing branch feature/new-settings-ui (sha: 7caa8d)
    Synced up to commit hash: https://github.com/Yoast/wordpress-seo/commit/7caa8d335142ad276761f0cc18cab4123f195a5a
    
    Timestamp last commit: 2022-07-13T19:25:47+02:00
    
    Committer last commit: @igorschoester
    
    Commit message last commit:
    
    ---------------------------
    
    Add link params and links

--- src/integrations/settings-integration.php
+++ src/integrations/settings-integration.php
@@ -10,6 +10,7 @@ use WPSEO_Option_Titles;
 use WPSEO_Options;
 use WPSEO_Post_Type;
 use WPSEO_Replace_Vars;
+use WPSEO_Shortlinker;
 use Yoast\WP\SEO\Conditionals\Settings_Conditional;
 use Yoast\WP\SEO\Config\Schema_Types;
 use Yoast\WP\SEO\Helpers\Current_Page_Helper;
@@ -218,7 +219,9 @@ class Settings_Integration implements Integration_Interface {
 				'isPremium'      => $this->product_helper->is_premium(),
 				'isNetworkAdmin' => \is_network_admin(),
 				'isMainSite'     => \is_main_site(),
+				'siteUrl'        => \get_bloginfo( 'url' ),
 			],
+			'linkParams'           => WPSEO_Shortlinker::get_query_params(),
 			'postTypes'            => $this->get_post_types(),
 		];
 	}
