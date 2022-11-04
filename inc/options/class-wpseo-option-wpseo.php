415c16c3e65c4eeebd4c8568dcb4b876d0718039
GitHub Action <yoastbot@users.noreply.github.com>
2022-11-04 09:00:50 +0000

    Release 19.10-RC4
    Commit hash for tag: https://github.com/Yoast/wordpress-seo/commit/fd84147791d937edd17d899a64fbfc7c3298fc58
    
    Timestamp for tag: 2022-11-04T08:41:43Z
    
    Committer for tag: @
    
    Commit message last commit:
    
    ---------------------------
    
    Bump version to 19.10-RC4

--- inc/options/class-wpseo-option-wpseo.php
+++ inc/options/class-wpseo-option-wpseo.php
@@ -131,8 +131,6 @@ class WPSEO_Option_Wpseo extends WPSEO_Option {
 		'least_linked_ignore_list'                 => [],
 		'indexables_page_reading_list'             => [ false, false, false, false, false ],
 		'indexables_overview_state'                => 'dashboard-not-visited',
-		'last_known_public_post_types'             => [],
-		'last_known_public_taxonomies'             => [],
 	];
 
 	/**
@@ -402,8 +400,6 @@ class WPSEO_Option_Wpseo extends WPSEO_Option {
 				case 'most_linked_ignore_list':
 				case 'least_linked_ignore_list':
 				case 'indexables_page_reading_list':
-				case 'last_known_public_post_types':
-				case 'last_known_public_taxonomies':
 					$clean[ $key ] = $old[ $key ];
 
 					if ( isset( $dirty[ $key ] ) ) {
