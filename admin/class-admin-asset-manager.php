f41fc36d97a3bad66c41177ea4e61c3cad9bc91b
GitHub Action <yoastbot@users.noreply.github.com>
2023-01-10 09:56:16 +0000

    Release 19.14
    Commit hash for tag: https://github.com/Yoast/wordpress-seo/commit/0a1623546fed6266f466035dcf63d41209542060
    
    Timestamp for tag: 2023-01-10T09:44:25Z
    
    Committer for tag: @
    
    Commit message last commit:
    
    ---------------------------
    
    Bump version to 19.14 on free

--- admin/class-admin-asset-manager.php
+++ admin/class-admin-asset-manager.php
@@ -236,7 +236,6 @@ class WPSEO_Admin_Asset_Manager {
 			'classic-editor',
 			'post-edit',
 			'help-scout-beacon',
-			'redirect-old-features-tab',
 		];
 		$additional_dependencies = [
 			'analysis-worker'          => [ self::PREFIX . 'analysis-package' ],
