867200f8be5ee57491357a81534845f22b662a60
GitHub Action <vraja-pro@users.noreply.github.com>
2024-04-12 14:59:21 +0000

    Syncing branch feature/decouple-hidden-fields (sha: b97fa0)
    Synced up to commit hash: https://github.com/Yoast/wordpress-seo/commit/b97fa080615a3e743cb0441ddd1532eae631d014
    
    Timestamp last commit: 2024-04-12T16:52:20+02:00
    
    Committer last commit: @vraja-pro
    
    Commit message last commit:
    
    ---------------------------
    
    Merge branch '21141-decouple-hidden-fields-for-metabox' into feature/decouple-hidden-fields

--- inc/class-wpseo-meta.php
+++ inc/class-wpseo-meta.php
@@ -296,7 +296,7 @@ class WPSEO_Meta {
 
 		/**
 		 * Allow add-on plugins to register their meta fields for management by this class.
-		 * Calls to add_filter() must be made before plugins_loaded prio 14.
+		 * Calls to add_filter() must be made before plugins_loaded prio 14, use in an initialzer class.
 		 */
 		$extra_fields = apply_filters( 'add_extra_wpseo_meta_fields', [] );
 		if ( is_array( $extra_fields ) ) {
@@ -325,7 +325,7 @@ class WPSEO_Meta {
 					self::$meta_prefix . $key,
 					[
 						'sanitize_callback' => [ self::class, 'sanitize_post_meta' ],
-						'show_in_rest'      => ( $field_def['type'] ) ? true : false,
+						'show_in_rest'      => self::get_show_in_rest_args( $field_def['type'] ),
 						'auth_callback'     => static function () {
 							return current_user_can( 'edit_posts' );
 						},
@@ -356,6 +356,53 @@ class WPSEO_Meta {
 
 		add_filter( 'update_post_metadata', [ self::class, 'remove_meta_if_default' ], 10, 5 );
 		add_filter( 'add_post_metadata', [ self::class, 'dont_save_meta_if_default' ], 10, 4 );
+	}
+
+	/**
+	 * Get show_in_rest args for a meta field.
+	 *
+	 * @param string|null $type The type of the field.
+	 * @return bool|array<array<string>|callable|int> $args The show_in_rest args.
+	 */
+	public static function get_show_in_rest_args( $type ) {
+		if ( ! $type ) {
+			return false;
+		}
+		return [
+			'schema'           => [
+				'type' => 'string',
+			],
+			'prepare_callback' => [ self::class, 'prepare_value_for_rest_request' ],
+			'user_id'          => get_current_user_id(),
+		];
+	}
+
+	/**
+	 * Prepare value for rest api. Replaces the default `prepare_value` mathod in core file wp-includes/rest-api/fields/class-wp-rest-meta-fields.php.
+	 *
+	 * @param string                                   $value   The value.
+	 * @param WP_REST_Request                          $request The request object.
+	 * @param array<string|int|array<string|callable>> $args    The show_in_rest args.
+	 *
+	 * @return string The metadata value.
+	 */
+	public static function prepare_value_for_rest_request( $value, $request, $args ) {
+		if ( ! $args['user_id'] ) {
+			return null;
+		}
+
+		$current_user = get_userdata( $args['user_id'] );
+
+		if ( ! $current_user || ! $current_user->allcaps['edit_posts'] ) {
+			return null;
+		}
+		$schema = $args['schema'];
+
+		if ( is_wp_error( rest_validate_value_from_schema( $value, $schema ) ) ) {
+			return null;
+		}
+
+		return rest_sanitize_value_from_schema( $value, $schema );
 	}
 
 	/**
@@ -1024,7 +1071,7 @@ class WPSEO_Meta {
 	 * @param string $keyword The keyword to be counted.
 	 * @param int    $post_id The id of the post to which the keyword belongs.
 	 *
-	 * @return array
+	 * @return array<int>
 	 */
 	public static function keyword_usage( $keyword, $post_id ) {
 
@@ -1074,9 +1121,9 @@ class WPSEO_Meta {
 	/**
 	 * Returns the post types for the given post ids.
 	 *
-	 * @param array $post_ids The post ids to get the post types for.
+	 * @param array<int> $post_ids The post ids to get the post types for.
 	 *
-	 * @return array The post types.
+	 * @return array<string> The post types.
 	 */
 	public static function post_types_for_ids( $post_ids ) {
 
